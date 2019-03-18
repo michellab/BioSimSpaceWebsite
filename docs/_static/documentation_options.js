648b53318fbfe63e48890ba8a7a64d99b15fdc87
BioSimSpaceBot <scratchmandoo@hotmail.com>
2019-03-18 12:59:00 +0000

    Updating website.

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2018.1.1+829.g554692f',
+    VERSION: '2018.1.1+830.g22ffd0e',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
